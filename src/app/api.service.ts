6ed15e9aa8a1e46b77731ef9e3be590bd29c0567
Tanish <tanish4643@yahoo.com>
2020-07-27 17:45:34 +0530

    Pagination & Comment Display

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -19,13 +19,15 @@ export class ApiService {
   BASE_URL = 'https://cors-anywhere.herokuapp.com/https://www.flickr.com/services/rest/?method=';
   FORMAT = '&format=json&nojsoncallback=?';
 
-  galleryURL = 'flickr.photos.search&api_key=' + this.API_KEY + '&media=photos&extras=url_c';
+  galleryURL = 'flickr.photos.search&api_key=' 
+                + this.API_KEY + '&media=photos&extras=url_c'
+                + '&per_page=30&page=';
   photoInfo = 'flickr.photos.getInfo&api_key=' + this.API_KEY + '&extras=url_c';
 
   constructor(private http: HttpClient) { }
 
-  getGallery(tag: String): any {
-    return  this.http.get<any>(this.BASE_URL+this.galleryURL+'&tags='+tag+this.FORMAT);
+  getGallery(tag: String,page: number): any {
+    return  this.http.get<any>(this.BASE_URL+this.galleryURL+page+'&tags='+tag+this.FORMAT);
   }
 
   getImageDetails(photo_id: String): any {
--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -14,18 +14,44 @@ export class GalleryComponent implements OnInit {
   gallery: Gallery[] = [];
   loading: boolean = true;
   error: boolean = false;
+  pageNumber: number = 1;
+  comments: any;
+  commentsArr: string[] = [];
 
   constructor(private api: ApiService) { }
 
   ngOnInit() {
+    this.fetchLocalComments();
     this.getGallery();
+  }
+
+  loadData(page: number){
+    this.pageNumber += page;
+    this.getGallery();
+  }
+
+  fetchLocalComments(){
+    var comments: any = localStorage.getItem('ratings');
+    
+    if(comments){
+      comments = JSON.parse(comments);
+      this.comments = comments;
+      comments = comments.map(item => item.id);
+      this.commentsArr = comments;
+      console.log(this.commentsArr);
+    }
+  }
+
+  counter(i: Number){
+    return Array(i);
   }
 
   getGallery(){
     this.loading = true;
     this.error = false;
+    this.gallery = [];
 
-    this.api.getGallery('food')
+    this.api.getGallery('food',this.pageNumber)
     .subscribe(data => {
       if(data.stat == "ok")
         for (const d of (data.photos.photo as any)) {
